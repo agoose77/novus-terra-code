50bc7d361f0be8a9ecf5dd11bd5ed4ae0fe376b2
andrew.t.bentley@gmail.com <andrew.t.bentley@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-06-26 06:35:01 +0000

    Adding FSM, kd tree, BST and entity base

--- EntityBase.py
+++
@@ -1,1 +0,0 @@
-import bge as bge
---
+++ finite_state_machine.py
@@ -0,0 +1,33 @@
+class FiniteStateMachine:
+"""Finite State Machine"""
+    def __init__(self, parent):
+        """Constructor
+        parent - arbitrary reference
+        """
+        self.parent = parent
+        self.states = {}
+        self.transitions = {}
+        self.current_state = None
+        
+    def add_state(self, state, action=None):
+        """Add another state
+        state - identifier
+        action - callable, to be run while state is active
+        """
+        self.states[state] = action
+        
+        if self.current_state is None:
+            self.current_state = state
+        
+    def add_transition(self, parent_state, child_state, condition, action=None):
+        self.transitions[parent_state] = [child_state, condition, action]
+        
+    def main(self):
+        for transition in self.transitions[self.current_state]:
+            if transition[1] (self):
+                self.current_state = transition[0]
+                if callable(transition[2]):
+                    transition[2] (self)
+        
+        if callable(self.states[self.current_state]):
+            self.states[self.currnet_state] (self)
