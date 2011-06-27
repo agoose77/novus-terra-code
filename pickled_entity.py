c3c5e4026264b58a95e55c8c4e69f4cdb8805fc8
andrew.t.bentley@gmail.com <andrew.t.bentley@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-06-27 11:59:59 +0000

    Various updates, will do a test tomorrow to see how the culling works.

--- pickled_entity.py
+++ pickled_entity.py
@@ -12,7 +12,7 @@ class PickledEntity:
         self.position = None
         self.orientation = None
         self.linear_velocity = None
-        self.rotational_velocity = None
+        self.angular_velocity = None
         self.frozen = None
         
     def add_to_world(self):
@@ -21,10 +21,15 @@ class PickledEntity:
     def remove_from_world(self):
         entity = EntityBase.entities[self.id]
         
-        self.position = entity.worldPosition
-        self.orientation = entity.worldOrientation
-        self.linear_velocity = entity.worldLinearVelocity
-        self.roatational_velocity = entity.worldRotationalVelocity
+        # Save position, rotation and velocity but as lists not Vectors
+        self.position = entity.worldPosition[:]
+        self.orientation = [\
+            entity.worldOrientation[0][:],
+            entity.worldOrientation[1][:],
+            entity.worldOrientation[2][:]]
+        self.linear_velocity = entity.worldLinearVelocity[:]
+        self.roatational_velocity = entity.worldRotationalVelocity[:]
+        
         self.frozen = entity.frozen
         
         entity.destroy()
