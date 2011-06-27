350da3aae14f862c579ba19ee573f28dc7bf6988
live.matt.a@gmail.com <live.matt.a@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-06-27 02:50:55 +0000

    fixed an indentation error on finite_state_machine.py
    added camera controls, worked on FSMs, cleaned coded
    
    created sound_manager.py, added some basic code

--- trunk/player.py
+++ trunk/player.py
@@ -1,6 +1,9 @@
 import bge as bge
 from entity_base import EntityBase
 from finite_state_machine import FiniteStateMachine
+import math
+from game import Game
+from sound_manager import SoundManager
 
 class Player(EntityBase):
 
@@ -21,18 +24,19 @@ class Player(EntityBase):
 		self.camera_on = True
 		self.walk_speed = 3
 		self.run_speed = 5
+		self.camera = self.children[0]
 		self.movement_state_machine = FiniteStateMachine(self)
 		self.movement_state_machine.add_state('walk', self.handle_walk_state) # add state
 		self.movement_state_machine.add_state('climb', self.handle_climb_state)
-		self.movement_state_machine.add_state('jump', self.handle_jump_state)
+		#self.movement_state_machine.add_state('jump', self.handle_jump_state)
 		self.movement_state_machine.add_state('fall', self.handle_fall_state)
-		self.movement_state_machine.add_state('land', self.handle_land_state)
+		#self.movement_state_machine.add_state('land', self.handle_land_state)
 		self.movement_state_machine.add_state('vehicle', self.handle_vehicle_state)
 		self.movement_state_machine.add_state('none', self.handle_none_state)
 
 
 	def handle_walk_state(self, FSM):
-
+		print ('WAlk')
 		###
 		keyboard = bge.logic.keyboard.events
 		vel = self.getLinearVelocity()
@@ -61,21 +65,20 @@ class Player(EntityBase):
 				move[2] = 10
 
 		###
+		from mathutils import Vector
 		com = vel[2]+move[2]
+		t1 = move[1] * self.worldOrientation[2].copy()
+
 		self.worldLinearVelocity = [move[1],move[0], com]
+
+		# Handle sound
 
 
 	def handle_climb_state(self, FSM):
 		pass
 
-	def handle_jump_state(self, FSM):
-		pass
-
 	def handle_fall_state(self, FSM):
-		pass
-
-	def handle_land_state(self, FSM):
-		pass
+		print ('fall')
 
 	def handle_vehicle_state(self, FSM):
 		pass
@@ -83,11 +86,31 @@ class Player(EntityBase):
 	def handle_none_state(self, FSM):
 		pass
 
-	def is_grounded(self, FSM):
-		pass
 
-	def is_not_grounded(self, FSM):
-		pass
+
+	def is_grounded(self, FSM):
+		pos2 = [self.position[0],self.position[1],self.position[2]-5]
+		ray2 = self.rayCast(pos2, self.position, 0, '', 0, 0, 0)
+
+		if ray2[0] == None:
+			grounded = False
+
+		else:
+			grounded = True
+
+		return grounded
+
+	def is_falling(self, FSM):
+		pos2 = [self.position[0],self.position[1],self.position[2]-5]
+		ray2 = self.rayCast(pos2, self.position, 0, '', 0, 0, 0)
+
+		if ray2[0] == None:
+			falling = True
+
+		else:
+			falling = False
+
+		return falling
 
 	def has_entered_ladder(self, FSM):
 		pass
@@ -101,11 +124,10 @@ class Player(EntityBase):
 	def has_exited_vehicle(self, FSM):
 		pass
 
-	def has_pressed_jumpkey(self, FSM):
-		pass
+	#def has_pressed_jumpkey(self, FSM):
+	#	pass
 
-	#
-	def has_entered_none(self, FSM): # for dialogs
+	def has_entered_none(self, FSM):
 		pass
 
 	def has_exited_none(self, FSM):
@@ -116,11 +138,33 @@ class Player(EntityBase):
 	def handle_interactions(self):
 		pass
 
-	def fast_travel(self, location_id):
-		pass
+	def fast_travel(self, location):
+		self.position = location_id.position
+		# handle world cells
 
 	def handle_camera(self):
-		pass
+		bge.logic.getCurrentScene().active_camera = self.camera # set active_camera
+
+		mpos = bge.logic.mouse.position
+
+		w = bge.render.getWindowWidth()
+		h = bge.render.getWindowHeight()
+
+		bge.render.setMousePosition(int(w/ 2), int(h/ 2))
+
+
+		if not 'ml_rotx' in self.camera:
+			self.camera['ml_rotx'] = -(self.camera.localOrientation.to_euler().x - (math.pi * 0.5))
+
+		else:
+			mouse_mx = (mpos[0] - 0.5) * Game.MOUSE_SENSITIVITY
+			mouse_my = (mpos[1] - 0.5) * Game.MOUSE_SENSITIVITY
+
+			if abs(mouse_mx) > 0.001 or abs(mouse_my) > 0.001:
+				self.camera.parent.applyRotation([0, 0, -mouse_mx], 0) # X
+				self.camera.applyRotation([-mouse_my, 0, 0], 1) # Y
+				self.camera['ml_rotx'] += mouse_my
+
 
 	def remove_controls(self):
 		self.stored_state = self.movement_state_machine.current_state
@@ -132,6 +176,12 @@ class Player(EntityBase):
 	def main(self):
 		EntityBase.main(self)
 		self.movement_state_machine.main()
+		self.handle_camera()
+
+		if self.movement_state_machine.current_state == 'walk':
+			self.movement_state_machine.add_transition('walk', 'fall', self.is_falling, self.handle_fall_state)
+		else:
+			self.movement_state_machine.add_transition('fall', 'walk', self.is_grounded, self.handle_walk_state)
 
 
 
