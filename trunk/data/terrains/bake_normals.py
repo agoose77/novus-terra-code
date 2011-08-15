997fff3d176b4878d8acd3b5eac215593068959e
joseph.parker.1@gmail.com <joseph.parker.1@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-08-15 01:25:28 +0000

    novus.terrain

--- trunk/data/terrains/bake_normals.py
+++ trunk/data/terrains/bake_normals.py
@@ -23,11 +23,14 @@ if __name__ == '__main__':
 	try:
 		file = open( 'crosscrater_norms.txt', 'rb')
 		norms = []
+		i = 0
 		for line in file:
 			if line:
+				i += 1
+				print("reading line ", i)
 				norms += eval(line)
 		file.close()		
-				
+		print(len(norms))
 		for i in range(len(norms)):
 			norms[i] = (norms[i][0]+127, norms[i][1]+127, norms[i][2]+127)
 		#print norms[0]
