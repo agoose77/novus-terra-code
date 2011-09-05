013578c24828b50dda103529d0cd989323eb8188
joseph.parker.1@gmail.com <joseph.parker.1@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-09-05 17:21:40 +0000

    paths

--- trunk/src/paths.py
+++ trunk/src/paths.py
@@ -1,6 +1,17 @@
 # filepath constants
 
-import bge
+try:
+	import bge
+except:
+	pass
 
-PATH_SOUNDS = bge.logic.expandPath("//data//sounds\\")
+PATH_SOUNDS = bge.logic.expandPath("//sounds\\")
 PATH_MUSIC = bge.logic.expandPath("//music\\")
+
+def safepath(filename):
+	filename = filename.replace("/", "\\")
+	filename = filename.replace(".\\","//")
+	return bge.logic.expandPath(filename)
+	
+def safeopen(filename, arg1):
+	return open(safepath(filename), arg1)
