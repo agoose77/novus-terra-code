df24d282a42cbfcb8e3966c229a84bd7ca0766ec
live.matt.a@gmail.com <live.matt.a@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-11-08 05:29:27 +0000

    missed a few files from last commit

---
+++ src/weapon_pickup.py
@@ -0,0 +1,25 @@
+### Weapon Pickup
+import sys
+sys.path.append('./src/')
+
+import bge
+from mathutils import Vector, Matrix
+import random
+import session
+from entity_base import EntityBase
+
+###
+class WeaponPickup(EntityBase):
+
+	def __init__(self, packet=None):
+		print ("WeaponPickup.__init__()")
+		EntityBase.__init__(self, packet)
+
+		self.iteract_label = 'pickup'
+		self.interact_info = "P90 - Weapon"
+		self.weapon_name = "P90"
+
+	def on_interact(self, player):
+		player.inventory.replace_weapon(self.weapon_name)
+		new = player.inventory.weapon_slot_1.equip()
+		self.endObject()
