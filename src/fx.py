31d7a5b3c339da872336bfff6520a78d376f7fe8
live.matt.a@gmail.com <live.matt.a@gmail.com@695d9975-9c34-887d-dab2-a7d4427b3823>
2011-12-05 23:39:15 +0000

    fx

---
+++ src/fx.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('./src/')
+
+import bge
+import session
+
+class Effects:
+	def __init__(self):
+		print("fx.__init__()")
+		# load saved_settings
+
+
+
+
+	def update_fog():
+		pass
+
+	def update_other():
+		pass
